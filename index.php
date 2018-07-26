c591b28ef9b8f35eb8315e79925bef01a5a024e9
Nishan B <nishanb7@gmail.com>
2018-07-26 21:06:45 +0530

    fixed in small device

--- index.php
+++ index.php
@@ -8,34 +8,31 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/style.css">
+
     <link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://unpkg.com/scrollreveal/dist/scrollreveal.min.js"></script>
-    <script src="js/bootstrap.min.js" crossorigin="anonymous"></script>
 </head>
 <body>
 
 <section id="main">
-    <div id="container"><!-- flex container -->
-
+    <div id="container">
 
         <div class="box img-s">
-            <img class="" src="img/logo1.png">
+            <img class="img-fluid" src="img/logo1.png">
         </div>
 
-        <br>
         <div class="box text-s" id="main-text">
             <h1>We're Coming Soon</h1>
-            <h5>Maintained By <a href="youflik.com">Youflik Web Devlopment Studio</a></h5>
+            <br>
+            <h6>Maintained By <a href="http://youflik.com" class="btn-link">Youflik Web Devlopment Studio</a></h6>
         </div>
-
 
     </div>
 </section>
 
-<!-- Optional JavaScript -->
-
+<!--  JavaScript -->
 <script>
     window.sr = ScrollReveal();
     sr.reveal('.img-s', {duration: 2000, origin: 'top', distance: '200px'});
