02936ceb0e85ae71ec9166ae7d62c5a7ce5e8195
Nishan B <nishanb7@gmail.com>
2018-08-02 16:13:19 +0530

    second release

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/style.css">
 
+    <link href="https://fonts.googleapis.com/css?family=Varela" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
@@ -22,13 +23,13 @@
         <div class="box img-s">
             <img class="img-fluid" src="img/logo_main.png">
             <br/>
-            <div class="text-capitalize " style="font-size: 4rem;color: #4169e2!important;margin-top: -1rem;">Car Care</div>
-            <p style="color:#95C535!important;">Care that your car deserves</p>
+            <div class="text-capitalize c-txt " style="font-size: 4rem;color: #4169e2!important;margin-top: -1rem;">Car Care</div>
+            <h5 style="color:#95C535!important;" class="c-txt">Care that your car deserves</h5>
             <br/>
         </div>
 
         <div class="box text-s" id="main-text">
-            <h3>We're Coming Soon</h3>
+            <h3 class="c-txt">We're Coming Soon</h3>
             <br>
             <h6>Maintained By <a href="http://youflik.com" class="btn-link">Youflik Web Devlopment Studio</a></h6>
         </div>
