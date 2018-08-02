c585d4eea8c1a4c3c96214d1a25b7b37cfbb79a8
Nishan B <nishanb7@gmail.com>
2018-08-02 21:13:53 +0530

    added logo and insta link

--- index.php
+++ index.php
@@ -4,13 +4,12 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/style.css">
-
     <link href="https://fonts.googleapis.com/css?family=Varela" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://unpkg.com/scrollreveal/dist/scrollreveal.min.js"></script>
@@ -23,15 +22,24 @@
         <div class="box img-s">
             <img class="img-fluid" src="img/logo_main.png">
             <br/>
-            <div class="text-capitalize c-txt " style="font-size: 4rem;color: #4169e2!important;margin-top: -1rem;">Car Care</div>
+            <div class="text-capitalize c-txt " style="font-size: 3.0rem;color: #4169e2!important;margin-top: -1rem;">
+                Car Care
+            </div>
             <h5 style="color:#95C535!important;" class="c-txt">Care that your car deserves</h5>
             <br/>
         </div>
 
         <div class="box text-s" id="main-text">
             <h3 class="c-txt">We're Coming Soon</h3>
-            <br>
-            <h6>Maintained By <a href="http://youflik.com" class="btn-link">Youflik Web Devlopment Studio</a></h6>
+            <h6>Maintained By <a href="http://youflik.com" class="btn-link" style="text-decoration: none;">Youflik Web Development Studio</a></h6>
+        </div>
+
+        <div style="position:fixed; width:100%; height:80px;padding:5px; bottom:0px;" class="text-center text-s">
+            <a href="https://www.instagram.com/krithicarcare"><i style="font-size:24px" class="fa">&#xf16d;</i></a>
+        </div>
+
+        <div style="position:fixed; width:100%; height:40px;padding:5px; bottom:0px;" class="text-center text-s">
+            <span class="small">Krithi car care division of krithi automobiles pvt ltd</span>
         </div>
 
     </div>
@@ -40,9 +48,8 @@
 <!--  JavaScript -->
 <script>
     window.sr = ScrollReveal();
-    //sr.reveal('.container', {duration: 2000, origin: 'top', distance: '200px'});
+    sr.reveal('.img-s', {duration: 2000, origin: 'top', distance: '200px'});
     sr.reveal('.text-s', {duration: 2000, origin: 'bottom', distance: '200px'});
-
 </script>
 </body>
 </html>
